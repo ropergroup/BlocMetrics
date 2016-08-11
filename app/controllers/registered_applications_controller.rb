e2d7fa9f45b6aa226cd36a3ed7136677815a3c0b
Chris Roper <chris@roper.xyz>
2016-08-11 14:49:42 -0600

    regapp controller and views

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -1,20 +1,64 @@
 class RegisteredApplicationsController < ApplicationController
 
   def index
-
     @registered_applications =RegisteredApplication.all
   end
 
   def show
+    @registered_applications = RegisteredApplication.find(params[:id])
   end
 
-  def new
+  def create
+
+      @registered_applications = RegisteredApplication.new
+      @registered_applications.appname = params[:registered_applications][:appname]
+      @registered_applications.url = params[:registered_applications][:url]
+
+      if @registered_applications.save
+
+        flash[:notice] = "Application was saved successfully."
+        redirect_to @registered_applications
+      else
+
+        flash.now[:alert] = "There was an error saving the application. Please try again."
+        render :new
+      end
+  end
+
   end
 
   def edit
+    @registered_applications = RegisteredApplication.find(params[:id])
   end
 
+  def update
+     @registered_applications = RegisteredApplication.find(params[:id])
+     @registered_applications.title = params[:registered_applications][:appname]
+     @registered_applications.body = params[:registered_applications][:url]
+
+     if @registered_applications.save
+       flash[:notice] = "The application was updated successfully."
+       redirect_to @registered_applications
+     else
+       flash.now[:alert] = "There was an error saving the application. Please try again."
+       render :edit
+     end
+   end
+
   def destroy
+      @user = current_user
+      @registered_applications = @user.registered_applications.find(params[:id])
+
+    if @registered_applications.destroy
+      flash[:notice] = "The application has been completed!"
+    else
+      flash[:alert] = "There was an error deleting the application. Try again."
+    end
+
+      respond_to do |format|
+        format.html
+        format.js
+      end
   end
 
 end
