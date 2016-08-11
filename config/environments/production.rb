2abe3dfe170133678630e3c7844b28c51fa8bf32
Chris Roper <chris@roper.xyz>
2016-08-11 10:13:26 -0600

    confirmable setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {:host => 'arcane-garden-26158.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port    => 2525,
+  :domain  => 'heroku.com'
+                              }
 end
