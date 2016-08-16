db9410c171c673f4821f9506ed64d0ff9217dd93
Chris Roper <chris@roper.xyz>
2016-08-15 19:00:28 -0600

    fixed Rspec controller name

--- spec/controllers/registered_applications_controller_spec.rb
+++ spec/controllers/registered_applications_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RegisteredApplication, type: :controller do
+RSpec.describe RegisteredApplicationsController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   before (:each) do
@@ -9,14 +9,14 @@ RSpec.describe RegisteredApplication, type: :controller do
   end
 
   describe "create" do
-    it "creates registered application" do
-      expect { post :create, :registered_application => {appname: "awesome app"}, user_id: @user.id }.to change(RegisteredApplication, :count).by(1)
+   it "creates registered application" do
+      expect { post :create, user_id: @user.id, id: registered_application.id, post: {appname: "awesome app", url: "www.test.com"}  }.to change(RegisteredApplication, :count).by(1)
     end
   end
 
   describe "destroy" do
-    it "creates registered_application" do
-      registered_application = FactoryGirl.create(:registered_application, user_id: @user.id)
+   it "creates registered_application" do
+     registered_application = FactoryGirl.create(:registered_application, user_id: @user.id)
       expect { delete :destroy, user_id: @user.id, id: registered_application.id }.to change(RegisteredApplication, :count).by(-1)
     end
   end
